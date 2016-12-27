873f0258ef84c1c26be64381871c8e205fec41dd
jnohboy <845854249@qq.com>
2016-12-27 18:46:52 +0800

    枯

--- ionic-project/js/config.js
+++
@@ -1,14 +0,0 @@
-/**
- * Created by chris on 2016/12/23.
- */
-angular.module('myapp.config', [])
-.constant('G', {
-    'api': 'http://www.phonegap100.com/appapi.php', 
-    'siteurl': 'http://www.phonegap100.com', 
-    'imgUrl': 'http://www.phonegap100.com/data/attachment/'
-})
-
-//登录的接口: http://www.phonegap100.com/appapi.php?a=login2
-//是post请求
-// 帐号
-// account: 'chrishu', password: 'a123456789'
